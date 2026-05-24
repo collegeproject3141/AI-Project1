d4d39f838292033cdfaf7ad70193a0dc5d34aa8c
abu <abuzar.qzq@gmail.com>
2026-05-25 02:02:40 +0530

    4th commit

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 const upload = multer({
@@ -164,7 +168,7 @@ app.post("/upload-image", upload.single("image"), async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
